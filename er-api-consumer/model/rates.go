f6a74400cd9e9566e11deda6cfbfd9694d99ef33
JimySheepman <y.alikoyuncu@gmail.com>
2022-04-17 15:46:38 +0300

    read data and consumer fixed

--- er-api-consumer/model/rates.go
+++ er-api-consumer/model/rates.go
@@ -16,5 +16,5 @@ type Currencies struct {
 	BaseCode       string  `json:"base_code"`
 	TargetCode     string  `json:"target_code"`
 	ConversionRate float64 `json:"conversion_rate"`
-	CreatedAt      string  `json:"create_at"`
+	CreatedAt      int64   `json:"create_at"`
 }
--- er-api-consumer/task/task.go
+++ er-api-consumer/task/task.go
@@ -1,35 +1,34 @@
 package task
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"er-api-consumer/model"
-
-	"github.com/streadway/amqp"
 )
 
 const (
-	MAIN_PATH = "https://v6.exchangerate-api.com/v6/"
-	SUB_PATH  = "/latest/"
+	MAIN_PATH   = "https://v6.exchangerate-api.com/v6/"
+	LATEST_PATH = "/latest/"
+	PAIR_PATH   = "/pair/"
 )
 
 var queryPaths = [3]string{"TRY", "USD", "EUR"}
 
 func Task() {
-	var rates model.Rates
-	_ = rates
-
 	for _, path := range queryPaths {
-		fecthValue := GetValueFromAPI(path)
-		Send(fecthValue)
+		fecthValue := GetAllValueFromAPI(LATEST_PATH, path)
+		unmarshalData := UnmarshalValueForRates(fecthValue)
+		SentToQueueByPairvalue(unmarshalData)
 	}
 }
 
-func GetValueFromAPI(path string) []byte {
-	res, err := http.Get(MAIN_PATH + os.Getenv("API_KEY") + SUB_PATH + path)
+func GetAllValueFromAPI(subPath string, path string) []byte {
+	res, err := http.Get(MAIN_PATH + os.Getenv("API_KEY") + subPath + path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,50 +38,35 @@ func GetValueFromAPI(path string) []byte {
 		log.Fatal("Could not complete read from request body")
 	}
 
-	/* 	err = json.Unmarshal(req, &rates)
-	   	if err != nil {
-	   		log.Fatal("Could not complete unmarshal body")
-	   	}
-	*/
-	//fmt.Printf("%s", req)
 	return req
 }
 
-func Send(fecthValue []byte) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+func UnmarshalValueForRates(req []byte) model.Rates {
+	var rates model.Rates
+	err := json.Unmarshal(req, &rates)
+	if err != nil {
+		log.Fatal("Could not complete unmarshal body")
+	}
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	//body := "Hello World!"
-	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        fecthValue,
-		})
-	failOnError(err, "Failed to publish a message")
-	log.Printf(" [x] Sent %s\n", fecthValue)
+	return rates
 }
 
-func failOnError(err error, msg string) {
+func UnmarshalValueForCurrencies(req []byte) model.Currencies {
+	var currencies model.Currencies
+	err := json.Unmarshal(req, &currencies)
 	if err != nil {
-		log.Panicf("%s: %s", msg, err)
+		log.Fatal("Could not complete unmarshal body")
+	}
+
+	return currencies
+}
+
+func SentToQueueByPairvalue(rates model.Rates) {
+	for target := range rates.ConversionRates {
+		value := GetAllValueFromAPI(PAIR_PATH, rates.BaseCode+"/"+target)
+		data := UnmarshalValueForCurrencies(value)
+		data.CreatedAt = time.Now().UTC().Unix()
+		Send(data, rates.BaseCode)
+		time.Sleep(100 * time.Millisecond)
 	}
 }
